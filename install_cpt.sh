e45e9d01b767d3946d4afc8d4ab8e742f9c15f75
randifilan <randimaskil9@gmail.com>
2019-10-29 08:58:01 +0700

    Update install_cpt.sh

--- install_cpt.sh
+++ install_cpt.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+cd /tmp
+wget -O cpt.tar.gz https://www.netacad.com/portal/sites/default/files/resources/PacketTracer/packet_tracer_7.1.1_for_linux_64_bit.tar.gz
+apt -y install tar
+mkdir /opt/pt
+tar -xvzf cpt.tar.gz
