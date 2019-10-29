2ef30df99fbf8deb808a7e086d0f2d1b02e302f8
randifilan <randimaskil9@gmail.com>
2019-10-29 09:09:25 +0700

    Update install_cpt.sh

--- install_cpt.sh
+++ install_cpt.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 cd /tmp
+sudo apt update && apt -y install tar wget libqt5webkit5 libqt5multimediawidgets5 libqt5svg5 libqt5script5 libqt5scripttools5 libqt5sql5
 wget -O cpt.tar.gz https://www.netacad.com/portal/sites/default/files/resources/PacketTracer/packet_tracer_7.1.1_for_linux_64_bit.tar.gz
-apt -y install tar
 mkdir /opt/pt
-tar -xvzf cpt.tar.gz
+tar -xvzf cpt.tar.gz -C /opt/pt
+wget -O libpng.deb http://ftp.br.debian.org/debian/pool/main/libp/libpng/libpng12-0_1.2.50-2+deb8u3_amd64.deb
+wget -O libicu.deb http://mirrors.kernel.org/ubuntu/pool/main/i/icu/libicu52_52.1-3ubuntu0.8_amd64.deb
+apt -y install ./libpng.deb
+apt -y install ./libicu.deb
+
+
+
+
